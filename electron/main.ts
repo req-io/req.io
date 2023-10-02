a394c8a4794803bab66a7028f68d7254e3f761c0
kannananil <56071561+kannananil@users.noreply.github.com>
2023-10-02 14:50:15 +0530

    [ Anil ] Remove unused template code and assets

--- electron/main.ts
+++ electron/main.ts
@@ -20,7 +20,7 @@ const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(process.env.VITE_PUBLIC, 'reqio.svg'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
