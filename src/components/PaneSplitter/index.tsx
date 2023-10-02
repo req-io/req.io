da2614a840fa0e3c22a92315f1deae2717bcc3eb
kannananil <56071561+kannananil@users.noreply.github.com>
2023-10-02 23:50:30 +0530

    [ Anil ] Added a bi directional reusable pane splitter component

---
+++ src/components/PaneSplitter/index.tsx
@@ -0,0 +1,11 @@
+import './index.scss'
+
+export type PaneSplitterProps = {
+  direction: string;
+}
+
+const PaneSplitter = (props: PaneSplitterProps) => {
+  return <div className={`pane-splitter-${props.direction}`}></div>;
+};
+
+export default PaneSplitter;
