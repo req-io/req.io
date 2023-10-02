68c6135f75581a44c3daa8fa0535224b3e8028bf
kannananil <56071561+kannananil@users.noreply.github.com>
2023-10-02 23:51:01 +0530

    [ Anil ] Added Home page with basic layout of the applicaiton

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
 import './index.scss'
+import Sidebar from "../../components/Sidebar";
+import Container from "../../components/Container";
+import Header from "../../components/Header";
+import PaneSplitter from "../../components/PaneSplitter";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Reqio</h1>
-      <p>A simple native desktop HTTP client</p>
+    <div className='home'>
+      <Header/>
+      <PaneSplitter direction='vertical'/>
+      <div className='main-container'>
+        <Sidebar/>
+        <PaneSplitter direction='horizontal'/>
+        <Container/>
+      </div>
     </div>
   );
 }
