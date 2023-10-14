de17a50374e679c4855c168d5509938a324c8569
Kannan Anil <56071561+kannananil@users.noreply.github.com>
2023-10-14 14:12:55 +0530

    [ Anil ] Updated the color theme of the app, standardised the color namings

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,13 +2,18 @@ import './index.scss';
 import RequestPanel from "../RequestPanel";
 import ResponsePanel from "../ResponsePanel";
 import PaneSplitter from "../PaneSplitter";
+import UrlPanel from "../UrlPanel";
 
 
 const Container = () => {
     return <div className='container'>
-        <RequestPanel/>
-        <PaneSplitter direction='horizontal'/>
-        <ResponsePanel/>
+        <UrlPanel/>
+        <PaneSplitter direction='vertical'/>
+        <div className='sub-container'>
+            <RequestPanel/>
+            <PaneSplitter direction='horizontal'/>
+            <ResponsePanel/>
+        </div>
     </div>
 };
 
