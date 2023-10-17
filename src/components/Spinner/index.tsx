f74f44e0b13efb7450cbf05b0cef99693bff53b6
Kannan Anil <56071561+kannananil@users.noreply.github.com>
2023-10-18 00:43:54 +0530

    [ Anil ] Added a simple Spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const Spinner = () => {
+  return (
+    <div className='spinner-container'>
+      <div className='spinner'></div>
+    </div>
+  );
+};
+
+export default Spinner;
