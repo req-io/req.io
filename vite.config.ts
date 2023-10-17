05848cbb2c79c1dbf14a5dcdf7df558ae762a39f
Kannan Anil <56071561+kannananil@users.noreply.github.com>
2023-10-18 00:45:13 +0530

    [ Anil ] Update vite config, added svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +22,7 @@ export default defineConfig({
       // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
       renderer: {},
     }),
+    svgr()
   ],
   css: {
     preprocessorOptions: {
