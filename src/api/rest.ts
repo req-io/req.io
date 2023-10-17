5161f64ead22035a381a33205ef362333618bd40
Kannan Anil <56071561+kannananil@users.noreply.github.com>
2023-10-18 02:04:22 +0530

    [ Anil ] Added api gateway method for post request

--- src/api/rest.ts
+++ src/api/rest.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
 
+type JSONValue =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: JSONValue }
+  | Array<JSONValue>;
 
 export const get = async (url: string) => {
   return axios.get(url);
+}
+
+export const post = async (url: string, body: JSONValue) => {
+  return axios.post(url, body);
 }
 
