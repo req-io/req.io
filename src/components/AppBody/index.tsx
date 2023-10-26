b26b0cfa166bf09989081ff52dbb39cc2b1382bf
pravithaop <pravithaop@Pravithas-MacBook-Pro.local>
2023-10-26 13:15:35 +0530

    [17] | Pravitha | Resolved missing imports caused by merge conflict

--- src/components/AppBody/index.tsx
+++ src/components/AppBody/index.tsx
@@ -4,99 +4,99 @@ import ResponsePanel from "../ResponsePanel";
 import PaneSplitter from "../PaneSplitter";
 import UrlPanel from "../UrlPanel";
 import { get, patch, post } from "../../api/rest.ts";
-import { getStatusText } from "../../api/statusCodes.ts";
+import { getErrorCode, getHttpStatusText } from "../../api/statusCodes.ts";
 
 import { useState } from "react";
 import { AxiosError, AxiosResponse } from "axios";
 
 
 const AppBody = () => {
-  const [ method, setMethod ] = useState('GET');
-  const [ url, setUrl ] = useState('');
-  const [ headers, setHeaders ] = useState([ { key: 'Content-Type', value: 'application/json' } ]);
-  const [ body, setBody ] = useState('');
-  const [ isNoRequestTriggered, setIsNoRequestTriggered ] = useState(true);
-  const [ isLoading, setIsLoading ] = useState(false);
-  const [ response, setResponse ] = useState('');
-  const [ statusCode, setStatusCode ] = useState(0);
-  const [ statusText, setStatusText ] = useState('');
+    const [ method, setMethod ] = useState('GET');
+    const [ url, setUrl ] = useState('');
+    const [ headers, setHeaders ] = useState([ {key: 'Content-Type', value: 'application/json'} ]);
+    const [ body, setBody ] = useState('');
+    const [ isNoRequestTriggered, setIsNoRequestTriggered ] = useState(true);
+    const [ isLoading, setIsLoading ] = useState(false);
+    const [ response, setResponse ] = useState('');
+    const [ statusCode, setStatusCode ] = useState(0);
+    const [ statusText, setStatusText ] = useState('');
 
-  const onSuccessResponse = (response: AxiosResponse) => {
-    setIsLoading(false);
-    setResponse(JSON.stringify(response.data, null, 2));
-    setStatusCode(response.status);
-    setStatusText(response.statusText || getHttpStatusText(response.status));
-  }
-
-  const onFailureResponse = (error: AxiosError) => {
-    setIsLoading(false);
-    if (error.response) {
-      setResponse(JSON.stringify(error.response?.data || {}, null, 2));
-      setStatusCode(error.response?.status || 0);
-      setStatusText(error.response?.statusText || getHttpStatusText(statusCode));
-    } else {
-      setResponse(`Error: ${ error.message }`);
-      setStatusText(`ERROR: ${ getErrorCode(error?.code || '') }`);
-      setStatusCode(0);
+    const onSuccessResponse = (response: AxiosResponse) => {
+        setIsLoading(false);
+        setResponse(JSON.stringify(response.data, null, 2));
+        setStatusCode(response.status);
+        setStatusText(response.statusText || getHttpStatusText(response.status));
     }
-  }
 
-  const onSend = () => {
-    if (method === 'GET' && url !== '') {
-      setIsNoRequestTriggered(false);
-      setIsLoading(true);
-      get(url, headers)
-        .then(onSuccessResponse)
-        .catch(onFailureResponse);
+    const onFailureResponse = (error: AxiosError) => {
+        setIsLoading(false);
+        if (error.response) {
+            setResponse(JSON.stringify(error.response?.data || {}, null, 2));
+            setStatusCode(error.response?.status || 0);
+            setStatusText(error.response?.statusText || getHttpStatusText(statusCode));
+        } else {
+            setResponse(`Error: ${ error.message }`);
+            setStatusText(`ERROR: ${ getErrorCode(error?.code || '') }`);
+            setStatusCode(0);
+        }
     }
-    if (method === 'POST' && url !== '') {
-      setIsNoRequestTriggered(false);
-      setIsLoading(true);
-      post(url, JSON.parse(body), headers)
-        .then(onSuccessResponse)
-        .catch(onFailureResponse);
+
+    const onSend = () => {
+        if (method === 'GET' && url !== '') {
+            setIsNoRequestTriggered(false);
+            setIsLoading(true);
+            get(url, headers)
+                .then(onSuccessResponse)
+                .catch(onFailureResponse);
+        }
+        if (method === 'POST' && url !== '') {
+            setIsNoRequestTriggered(false);
+            setIsLoading(true);
+            post(url, JSON.parse(body), headers)
+                .then(onSuccessResponse)
+                .catch(onFailureResponse);
+        }
+        if (method === 'PATCH' && url !== '') {
+            setIsNoRequestTriggered(false);
+            setIsLoading(true);
+            patch(url, JSON.parse(body), headers)
+                .then(onSuccessResponse)
+                .catch(onFailureResponse);
+        }
     }
-    if (method === 'PATCH' && url !== '') {
-      setIsNoRequestTriggered(false);
-      setIsLoading(true);
-      patch(url, JSON.parse(body), headers)
-          .then(onSuccessResponse)
-          .catch(onFailureResponse);
+
+    const onNewHeaderAddition = (header: Header) => {
+        setHeaders([ ...headers, header ])
     }
-  }
 
-  const onNewHeaderAddition = (header: Header) => {
-    setHeaders([ ...headers, header ])
-  }
+    const onHeadersChange = (updatedHeaders: Header[]) => {
+        setHeaders(updatedHeaders)
+    }
 
-  const onHeadersChange = (updatedHeaders: Header[]) => {
-    setHeaders(updatedHeaders)
-  }
-
-  return (
-    <div className='app-body'>
-      <UrlPanel
-        method={ method } url={ url }
-        onMethodChange={ setMethod }
-        onUrlChange={ setUrl }
-        onSend={ onSend }
-      />
-      <div className='sub-container'>
-        <RequestPanel
-          method={ method } headers={ headers }
-          onHeadersChange={ onHeadersChange }
-          onNewHeaderAddition={ onNewHeaderAddition }
-          onBodyChange={ setBody }
-        />
-        <PaneSplitter direction='horizontal'/>
-        <ResponsePanel
-          isNoRequestTriggered={ isNoRequestTriggered }
-          isLoading={ isLoading } response={ response }
-          statusCode={ statusCode } statusText={ statusText }
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className='app-body'>
+            <UrlPanel
+                method={ method } url={ url }
+                onMethodChange={ setMethod }
+                onUrlChange={ setUrl }
+                onSend={ onSend }
+            />
+            <div className='sub-container'>
+                <RequestPanel
+                    method={ method } headers={ headers }
+                    onHeadersChange={ onHeadersChange }
+                    onNewHeaderAddition={ onNewHeaderAddition }
+                    onBodyChange={ setBody }
+                />
+                <PaneSplitter direction='horizontal'/>
+                <ResponsePanel
+                    isNoRequestTriggered={ isNoRequestTriggered }
+                    isLoading={ isLoading } response={ response }
+                    statusCode={ statusCode } statusText={ statusText }
+                />
+            </div>
+        </div>
+    );
 };
 
 export default AppBody;
