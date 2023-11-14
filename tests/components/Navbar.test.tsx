fd15553fa0f7cd33807917cc1a20b560767e7551
Keerthana K <keerthana.kasthuril@gmail.com>
2023-11-14 19:21:02 +0530

    [25] | KK | Test for navbar component

---
+++ tests/components/Navbar.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import Navbar from "../../src/components/Navbar";
+
+describe('Navbar', () => {
+    it('should display the items passed in props', () => {
+        const itemsConfig = [
+            { name: 'body', label: 'Body', isActive: false, onClick: () => ({}), },
+            { name: 'headers', label: 'Headers', isActive: true, onClick: () => ({}) },
+        ];
+        render(<Navbar items={itemsConfig} />);
+
+        expect(screen.queryByRole('list')).toBeInTheDocument();
+        const navItems = screen.getAllByRole('listitem');
+        expect(navItems).toHaveLength(2);
+        expect(navItems[0]).toHaveTextContent('Body');
+        expect(navItems[0]).not.toHaveClass('active');
+        expect(navItems[1]).toHaveTextContent('Headers');
+        expect(navItems[1]).toHaveClass('active');
+    });
+});
