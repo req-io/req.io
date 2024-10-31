f7f2a2a63ef824506361c19b2d5f2de4884a826c
apurvagurme <58025903+apurvagurme@users.noreply.github.com>
2024-10-31 12:53:39 +0530

    [#85] | [Apurva] | Add test cases for Snackbar component #95

--- tests/components/Snackbar.test.tsx
+++ tests/components/Snackbar.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import SnackBar from '../../src/components/SnackBar';
+import SnackBar from '../../src/components/Snackbar';
 import '@testing-library/jest-dom';
 
 describe('SnackBar Component', () => {
